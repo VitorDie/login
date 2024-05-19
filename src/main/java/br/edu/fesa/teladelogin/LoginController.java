6e9122b2231d129f4ec4c57d75ba049ea9b751ff
vitordie <vitordie@protonmail.ch>
2024-05-19 11:58:12 -0300

    Criado na Controller a parte para o cancelButon

---
+++ src/main/java/br/edu/fesa/teladelogin/LoginController.java
@@ -0,0 +1,18 @@
+package br.edu.fesa.teladelogin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    @FXML
+    private Button cancelButton;
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module br.edu.fesa.teladelogin {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens br.edu.fesa.teladelogin to javafx.fxml;
+    exports br.edu.fesa.teladelogin;
+}
